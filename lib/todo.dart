52975e0ac38aa3ef30a1544a7d2c264d06352d7c
Dalso <flskwhdhk@gmail.com>
2023-12-25 21:29:42 +0900

    hive 라이브러리를 사용하여 db 환경 구축

--- lib/todo.dart
+++ lib/todo.dart
@@ -1,52 +1,21 @@
-class Todo {
+import 'package:hive/hive.dart';
+
+part 'todo.g.dart';
+
+@HiveType(typeId: 0)
+class Todo extends HiveObject {
+  @HiveField(0)
+  int? id;
+  
+  @HiveField(1)
   String title;
+  
+  @HiveField(2)
   int dateTime;
 
-//<editor-fold desc="Data Methods">
   Todo({
+    this.id,
     required this.title,
     required this.dateTime,
   });
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      (other is Todo &&
-          runtimeType == other.runtimeType &&
-          title == other.title &&
-          dateTime == other.dateTime);
-
-  @override
-  int get hashCode => title.hashCode ^ dateTime.hashCode;
-
-  @override
-  String toString() {
-    return 'Todo{' + ' title: $title,' + ' dateTime: $dateTime,' + '}';
-  }
-
-  Todo copyWith({
-    String? title,
-    int? dateTime,
-  }) {
-    return Todo(
-      title: title ?? this.title,
-      dateTime: dateTime ?? this.dateTime,
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return {
-      'title': this.title,
-      'dateTime': this.dateTime,
-    };
-  }
-
-  factory Todo.fromMap(Map<String, dynamic> map) {
-    return Todo(
-      title: map['title'] as String,
-      dateTime: map['dateTime'] as int,
-    );
-  }
-
-//</editor-fold>
 }
