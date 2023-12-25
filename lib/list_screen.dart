36210f104182fc0cb296c0acf123eea37046234b
Dalso <flskwhdhk@gmail.com>
2023-12-25 21:05:06 +0900

    todo 객체 만들기

--- lib/list_screen.dart
+++ lib/list_screen.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:todo_list/create_screen.dart';
+import 'package:todo_list/todo.dart';
 
-class ListScreen extends StatelessWidget {
+class ListScreen extends StatefulWidget {
   const ListScreen({super.key});
+
+  @override
+  State<ListScreen> createState() => _ListScreenState();
+}
+
+class _ListScreenState extends State<ListScreen> {
+  final todos = [
+    Todo(title: 'title 1', dateTime: 12321),
+    Todo(title: 'title 2', dateTime: 12321),
+    Todo(title: 'title 3', dateTime: 12321),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -11,22 +23,18 @@ class ListScreen extends StatelessWidget {
         title: Text('Todo 리스트'),
       ),
       body: ListView(
-        children: [
-          ListTile(
-            title: Text('title 1'),
-            subtitle: Text('subTitle 1'),
-          ),
-          ListTile(
-            title: Text('title 2'),
-            subtitle: Text('subTitle 2'),
-          ),
-        ],
+        children: todos.map((e) {
+          return ListTile(
+            title:Text(e.title),
+            subtitle: Text('${e.dateTime}')
+          );
+        }).toList(),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
           Navigator.push(
               context,
-              MaterialPageRoute(builder: 
+              MaterialPageRoute(builder:
               (context) => const CreateScreen()),
           );
         },
